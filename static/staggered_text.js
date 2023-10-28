9b4621b6681b1360498accd5b72d18508d8500df
Aidan <aidan.brock.mine@outlook.com>
2023-10-28 21:28:50 +0100

    Changed staggered_text to scroll

--- static/staggered_text.js
+++ static/staggered_text.js
@@ -10,6 +10,7 @@ function staggeredTextOutput(text){
     for (let i = 0; i < text.length; i++){
         setTimeout(() =>{
         text_elt.innerHTML += text[i];
+        if (i > 308 && i % 77 == 0) document.getElementById('description').scrollTo(0, 10000)
         }, i * 20)
     }
 }
